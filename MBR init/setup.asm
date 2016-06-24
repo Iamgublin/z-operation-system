d6345c2f9b1adcf2d5880377540bf9d82aa1c263
Iamgublin <403760189@qq.com>
2016-06-24 23:18:55 +0800

    MBR

---
+++ MBR init/setup.asm
@@ -0,0 +1,52 @@
+; boot.asm
+; Copyright (c) 2009-2012 mik 
+; All rights reserved.
+
+
+; 这是一个空白模块示例，设为 setup 模块
+; 用于写入磁盘的第 2 个扇区 ！
+;
+
+%include "..\inc\support.inc"
+
+;
+; 模块开始点是 SETUP_SEG - 2，减 2 是因为要算上模块头的存放的“模块 size”
+; load_module 加载到 SETUP_SEG-2，实际效果是 SETUP 模块会被加载到“入口点”即：setup_entry
+;
+        org SETUP_SEG - 2
+        
+;
+; 在模块的开头 word 大小的区域里存放模块的大小，
+; load_module 会根据这个 size 加载模块到内存
+
+SETUP_BEGIN:
+
+setup_length    dw (SETUP_END - SETUP_BEGIN)            ; SETUP_END-SETUP_BEGIN 是这个模块的 size
+
+
+setup_entry:                                            ; 这是模块代码的入口点。        
+message0 db 0dh, 0ah,"Loading S.M.A.R.T........  complete!"
+message1 db 0dh, 0ah,"Welcome My Master:ZhaoLiZhou", 0dh, 0ah
+message db 0dh, 0ah,"   <__,.n\.    /  ,--/ >", 0dh, 0ah 
+        db "         ＼ ', !--‐-i  /  /'", 0dh, 0ah 
+        db "         /'/'      L//'\/'", 0dh, 0ah 
+		db "       /   /,   /|  ,　 ,  ',", 0dh, 0ah 
+		db "      /  / /-‐/  ｉ  L_ / \! i", 0dh, 0ah 
+		db "       / / 7/'/   /'/-//!/\|   |", 0dh, 0ah 
+		db "        !,/7 '@'     'i@ii|    |", 0dh, 0ah 
+		db "         |./'    _    ,,,, / |./    |", 0dh, 0ah 
+		db "       /'| i＞./,,__  _,./ /   .i   |", 0dh, 0ah 
+		db "         /'| | / k_７_//'\,  /.  |", 0dh, 0ah 
+		db "            | |/i 〈|/   i  ,./ |  i  |", 0dh, 0ah 
+		db "           .|/ / ｉ:     /!   ＼  |", 0dh, 0ah 
+		db "            k\>//    _,.//    //!", 0dh, 0ah 
+		db "            !'<//'T'', ＼ ''7'/r'", 0dh, 0ah 
+		db "            /'\L__|___i,___,//|/", 0dh, 0ah 
+		db "               /-,/  |___./", 0dh, 0ah 
+		db "               '/'    !_,.:", 0dh, 0ah 
+times 1024-($-$$) db 0
+
+
+SETUP_END:
+
+; end of setup        
